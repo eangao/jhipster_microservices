9b747df31ee7f5a1bb6371b01d59f0951ad99ce1
Elmar Angao <32320010+eangao@users.noreply.github.com>
2021-06-02 17:21:21 +0800

    jhipster gateway application

---
+++ gateway/src/main/java/com/appsdeveloper/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.appsdeveloper.repository;
+
+import com.appsdeveloper.domain.Authority;
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
+
+/**
+ * Spring Data R2DBC repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends R2dbcRepository<Authority, String> {}
---
+++ gateway/src/test/java/com/appsdeveloper/config/NoOpMailConfiguration.java
@@ -0,0 +1,25 @@
+package com.appsdeveloper.config;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
+
+import com.appsdeveloper.service.MailService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class NoOpMailConfiguration {
+
+    private final MailService mockMailService;
+
+    public NoOpMailConfiguration() {
+        mockMailService = mock(MailService.class);
+        doNothing().when(mockMailService).sendActivationEmail(any());
+    }
+
+    @Bean
+    public MailService mailService() {
+        return mockMailService;
+    }
+}
